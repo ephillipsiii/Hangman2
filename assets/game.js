e0c0871501b6484487f0901677305ab03eea73a9
Edford Phillips <ed.d.phillips.iii@gmail.com>
2018-08-03 15:16:44 -0600

    attempt at hangman2.0. inital creation and beginning of js functionality

---
+++ assets/game.js
@@ -0,0 +1,6 @@
+//array of words to be randomly chosen
+var wordArray = ["ipa", "brown", "stout", "saison", "blonde", "amber", "blackipa"];
+//saving a chosen word
+var chosenWord = "";
+//an array of empty letters in a chosen word
+var emptyChosenWord = [];
